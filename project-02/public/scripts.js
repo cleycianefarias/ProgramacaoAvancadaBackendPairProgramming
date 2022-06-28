8c13e4d3c591c87447a2fcde2b90341196772b01
Victória Karolina de Lima Cavalcante <victoriaklcavalcante@gmail.com>
2022-06-27 20:07:45 -0500

    Solução do backend

--- project-02/public/scripts.js
+++ project-02/public/scripts.js
@@ -1,13 +1,43 @@
-const ul = document.querySelector('ul')
+const ul = document.querySelector('tbody')
 const input = document.querySelector('input')
 const form = document.querySelector('form')
 
+const lista = document.getElementById("urls")
+
+const button = document.getElementById("botao_excluir")
+let contador = 1
+
 function addElement({ name, url }) {
-    //...
+    var campo = document.getElementById("texto")
+    var item = document.createElement("tr")
+    var item1 = document.createElement("td")
+    var item2 = document.createElement("td")
+    var item3 = document.createElement("td")
+    var item4 = document.createElement("button")
+    item4.setAttribute("id", "botao_excluir")
+    item4.setAttribute("value", contador)
+    item4.setAttribute("onclick", "removeElement()")
+    item.setAttribute("id", contador)
+    //item4.setAttribute("type", "button")
+    //item4.setAttribute("value", "Excluir")
+
+    item1.innerHTML = name
+    item2.innerHTML = url
+    item4.innerHTML = "Excluir"
+    item3.appendChild(item4)
+
+    item.appendChild(item1)
+    item.appendChild(item2)
+    item.appendChild(item3)
+
+    lista.appendChild(item)
+    contador++
 }
 
 function removeElement(element) {
-    //...
+    console.log(element.typeOf())
+    //const element = document.getElementById("demo");
+    //element.remove();
 }
 
 form.addEventListener('submit', (event) => {
